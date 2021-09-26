fe35632bcf8eeef0abc5aa2ab8f748c3f4144ebc
archlizix <archlizix@qq.com>
2021-09-26 22:00:52 +0800

    add hj2

---
+++ Cpp/hj2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(void)
+{
+	constexpr int max_str_size = 500;
+	char str[max_str_size];
+	char ch;
+	int num = 0;
+
+	cin.getline(str, max_str_size);
+	cin >> ch;
+
+	for (int i = 0; str[i] != '\0'; ++i) {
+		if (str[i] >= 'A' && str[i] <= 'Z') {
+			str[i] += 'a' - 'A';
+		}
+	}
+	if (ch >= 'A' && ch <= 'Z') {
+		ch += 'a' - 'A';
+	}
+
+	for (int i = 0; str[i] != '\0'; ++i) {
+		num += (str[i] == ch) ? 1 : 0;
+	}
+
+	cout << num;
+	return 0;
+}
