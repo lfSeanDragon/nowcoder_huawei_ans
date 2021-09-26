368cbe821ca74121c286fbd5608bf65f43d31f8e
archlizix <archlizix@qq.com>
2021-09-26 22:39:55 +0800

    add hj3

---
+++ Cpp/hj3.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(void)
+{
+	int N;
+	int num[1000];
+	int flag = 0;
+
+	while (cin >> N) {
+		for (int i = 0; i < N; ++i) {
+			cin >> num[i];
+		}
+		sort(num, num + N);
+
+		int last = num[0];
+		if (flag) {
+			cout << endl;
+		}
+		flag = 1;
+		cout << num[0];
+		for (int i = 1; i < N; ++i) {
+			if (num[i] != last) {
+				cout << endl << num[i];
+				last = num[i];
+			}
+		}
+	}
+
+	return 0;
+}
