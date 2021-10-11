b83eb52756e31217093144907b0f2db0615de13d
archlizix <archlizix@qq.com>
2021-10-11 13:22:15 +0800

    just commit

--- Cpp/hj1.cpp
+++ Cpp/hj1.cpp
@@ -1,3 +1,14 @@
+/*!
+    \file      hj1.cpp
+    \author    archlizix (archlizix@qq.com)
+    \brief     HJ1 字符串最后一个单词的长度
+    \version   1.0
+    \date      2021-10-11
+
+    \copyright Copyright (c) 2021
+
+*/
+
 #include<iostream>
 #include<string>
 
