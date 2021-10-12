f70cdae215acc9eef9280de6515711df3107df31
archlizix <archlizix@qq.com>
2021-10-12 16:14:15 +0800

    just commit

---
+++ Cpp/hj4.cc
@@ -0,0 +1,34 @@
+/*!
+    \file      hj4.cc
+    \author    Archlizix (archlizix@qq.com)
+    \brief     HJ4 字符串分隔
+    \version   1.0
+    \date      2021-10-12
+
+    \copyright Copyright (C) 2021 Archlizix
+
+*/
+
+#include<iostream>
+#include<string>
+
+int main(void)
+{
+	std::string s;
+
+	while (getline(std::cin, s)) {
+		int n = (s.size() - 1) / 8;
+
+		for (int i = 0; i < n; ++i) {
+			std::cout << s.substr(i * 8, 8) << std::endl;
+		}
+
+		std::cout << s.substr(n * 8, s.size() - n * 8);
+		for (int i = 0; i < 8 - s.size() + n * 8; ++i) {
+			std::cout << '0';
+		}
+		std::cout << std::endl;
+	}
+
+	return 0;
+}
