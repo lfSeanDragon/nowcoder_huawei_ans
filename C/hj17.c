f253b61c216ff29c7d44b411a4c2a07f4fc55174
archlizix <archlizix@qq.com>
2021-11-04 21:10:28 +0800

    add hj17

---
+++ C/hj17.c
@@ -0,0 +1,86 @@
+/*!
+    \file      hj17.c
+    \author    Archlizix (archlizix@qq.com)
+    \brief     HJ17 坐标移动
+    \version   1.0
+    \date      2021-10-30
+
+    \copyright Copyright (C) 2021 Archlizix
+
+    \note      switch系状态机。
+*/
+#include <stdio.h>
+
+int x = 0, y = 0;
+
+void move(char *op, int len)
+{
+	if (len != 2 && len != 3) {
+		return;
+	}
+	if (op[1] < '0' || op[1] > '9') {
+		return;
+	}
+	if (len == 3 && (op[2] < '0' || op[2] > '9')) {
+		return;
+	}
+
+	int num = op[1] - '0';
+	if (len == 3) {
+		num = num * 10 + (op[2] - '0');
+	}
+
+	switch (op[0]) {
+	case 'A':
+		x -= num;
+		break;
+	case 'S':
+		y -= num;
+		break;
+	case 'D':
+		x += num;
+		break;
+	case 'W':
+		y += num;
+		break;
+	default:
+		break;
+	}
+}
+
+int main(void)
+{
+	char ch;
+	char op[3];
+	int p = 0;
+	while (1) {
+		ch = getchar();
+		switch (ch) {
+		case ';':
+			move(op, p);
+			p = 0;
+			break;
+		case '\n':
+			move(op, p);
+			printf("%d,%d\n", x, y);
+			x = 0;
+			y = 0;
+			p = 0;
+			break;
+		case EOF:
+			move(op, p);
+			if (x || y) {
+				printf("%d,%d\n", x, y);
+			}
+			return 0;
+		default:
+			if (p < 3) {
+				op[p] = ch;
+			}
+			p++;
+			break;
+		}
+	}
+
+	return 0;
+}
